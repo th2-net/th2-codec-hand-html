2b8ec060adbc973923ab4bbec0b101398815a76f
Luka Tchumburidze <luka.tchumburidze@exactprosystems.com>
2020-12-15 14:51:28 +0400

    [TH2-1171] Moved message generation logic to the new class

--- src/main/java/com/exactpro/th2/codec/handhtml/processor/FixProcessor.java
+++ src/main/java/com/exactpro/th2/codec/handhtml/processor/FixProcessor.java
@@ -71,92 +71,13 @@ public class FixProcessor {
         //TODO: Use Dictionary for this purpose
         fieldMap = (Map) adjustCollections(fieldMap);
 
-        Message subMessage = generateSubMessage(fieldMap);
+        Message subMessage = FixMessageGenerator.generateSubMessage(fieldMap);
 
         if (subMessage == null) {
             throw new Exception("Processor could not process the hierarchy");
         }
 
-        return generateMessage(messageType, rawMessage.getMetadata(), jsonMap, subMessage);
-    }
-
-    /*
-        Following method produces main message,
-        which process method returns
-     */
-
-    private Message generateMessage (String messageType, RawMessageMetadata rawMessageMetadata, Map <String, String> fields, Message subMessage) {
-        Message.Builder builder = Message.newBuilder();
-
-        MessageMetadata metadata = MessageMetadata.newBuilder()
-                .setId(rawMessageMetadata.getId())
-                .setMessageType(messageType)
-                .setTimestamp(rawMessageMetadata.getTimestamp())
-                .build();
-
-        builder.setMetadata(metadata);
-
-        for (var field : fields.entrySet()) {
-            if (field.getKey().equals(configuration.getContentKey())) {
-                builder.putFields(configuration.getContentKey(), Value.newBuilder().setMessageValue(subMessage).build());
-            } else {
-                builder.putFields(field.getKey(), Value.newBuilder().setSimpleValue(field.getValue()).build());
-            }
-        }
-
-        return builder.build();
-    }
-
-    /*
-        Generates subMessage, which contains
-        actual fields and hierarchy from html table
-     */
-
-    private Message generateSubMessage (Map <String, Object> fields) {
-        return buildParsedMessage(fields).build();
-
-    }
-
-    private Message.Builder buildParsedMessage(Map<String, Object> fields) {
-        Map<String, Value> messageFields = new LinkedHashMap<>(fields.size());
-        for (Map.Entry<String, Object> entry : fields.entrySet()) {
-            messageFields.put(entry.getKey(), parseObj(entry.getValue()));
-        }
-
-        return Message.newBuilder().putAllFields(messageFields);
-    }
-
-    private Value parseObj(Object value) {
-        /*
-            If current value is Map-like
-             we should create new Message from it
-         */
-        if (value instanceof Map) {
-            Message.Builder msgBuilder = Message.newBuilder();
-            for (Map.Entry<?, ?> o1 : ((Map<?, ?>) value).entrySet()) {
-                msgBuilder.putFields(String.valueOf(o1.getKey()), parseObj(o1.getValue()));
-            }
-            return Value.newBuilder().setMessageValue(msgBuilder.build()).build();
-        }
-
-        if (value instanceof List) {
-            ListValue.Builder listValueBuilder = ListValue.newBuilder();
-            for (Object o : ((List<?>) value)) {
-
-                if (o instanceof Map) {
-                    Message.Builder msgBuilder = Message.newBuilder();
-                    for (Map.Entry<?, ?> o1 : ((Map<?, ?>) o).entrySet()) {
-                        msgBuilder.putFields(String.valueOf(o1.getKey()), parseObj(o1.getValue()));
-                    }
-                    listValueBuilder.addValues(Value.newBuilder().setMessageValue(msgBuilder.build()));
-                } else {
-                    listValueBuilder.addValues(Value.newBuilder().setSimpleValue(String.valueOf(o)));
-                }
-            }
-            return Value.newBuilder().setListValue(listValueBuilder).build();
-        } else {
-            return Value.newBuilder().setSimpleValue(String.valueOf(value)).build();
-        }
+        return FixMessageGenerator.generateMessage (messageType, rawMessage.getMetadata(), jsonMap, configuration.getContentKey(), subMessage);
     }
 
     /*
